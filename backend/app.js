407d306f6497e0cbe5fc05df546b1585d99364c2
Fati Pereira <53302048+GoldWomenAcademy@users.noreply.github.com>
2020-06-28 21:28:44 +0200

    update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,25 @@
 const express = require('express');
 const app = express();
 
+app.use((req, res) => {
+   res.json({ message: 'Votre requête a bien été reçue !' }); 
+});
+
+app.use('/api/stuff', (req, res, next) => {
+   const stuff = [
+     {
+       name: 'Livre1',
+       description: 'La description du produit',
+       price: 4900,
+       inStock: 'Produit en stock',
+     },
+     {
+      name: 'Livre2',
+      description: 'La description du produit',
+      price: 3200,
+      inStock: 'Produit en stock',
+     },
+   ];
+   res.status(200).json(stuff);
+ });
 module.exports = app
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
 const http = require('http');
 const app = require('./app');
 
+app.set('port', process.env.PORT || 3000);
+const server = http.createServer(app);
+
 const normalizePort = val => {
   const port = parseInt(val, 10);
 
@@ -19,6 +22,7 @@ const errorHandler = error => {
   if (error.syscall !== 'listen') {
     throw error;
   }
+
   const address = server.address();
   const bind = typeof address === 'string' ? 'pipe ' + address : 'port: ' + port;
   switch (error.code) {
@@ -45,3 +49,5 @@ server.on('listening', () => {
 });
 
 server.listen(port);
+
+server.listen(process.env.PORT || 3000);
